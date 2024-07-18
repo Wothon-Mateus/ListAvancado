7c1b624546cfa29ad1422bed12b99c7629acc675
Wothon-Mateus <13wothon@gmail.com>
2024-07-18 20:14:53 -0300

    Final eventos

--- js/scripts.js
+++ js/scripts.js
@@ -122,12 +122,77 @@ const filterTodos = (filterValue) => {
 
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
-
+  
     const inputValue = todoInput.value;
-
-    if(inputValue){
-        saveTodo(inputValue);
+  
+    if (inputValue) {
+      saveTodo(inputValue);
     }
+  });
+  
+  document.addEventListener("click", (e) => {
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+    let todoTitle;
+  
+    if (parentEl && parentEl.querySelector("h3")) {
+      todoTitle = parentEl.querySelector("h3").innerText || "";
+    }
+  
+    if (targetEl.classList.contains("finish-todo")) {
+      parentEl.classList.toggle("done");
+  
+      updateTodoStatusLocalStorage(todoTitle);
+    }
+  
+    if (targetEl.classList.contains("remove-todo")) {
+      parentEl.remove();
+  
+      // Utilizando dados da localStorage
+      removeTodoLocalStorage(todoTitle);
+    }
+  
+    if (targetEl.classList.contains("edit-todo")) {
+      toggleForms();
+  
+      editInput.value = todoTitle;
+      oldInputValue = todoTitle;
+    }
+  });
+  
+  cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleForms();
+  });
+  
+  editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    const editInputValue = editInput.value;
+  
+    if (editInputValue) {
+      updateTodo(editInputValue);
+    }
+  
+    toggleForms();
+  });
+  
+  searchInput.addEventListener("keyup", (e) => {
+    const search = e.target.value;
+  
+    getSearchedTodos(search);
+  });
+  
+  eraseBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  
+    searchInput.value = "";
+  
+    searchInput.dispatchEvent(new Event("keyup"));
+  });
+  
+  filterBtn.addEventListener("change", (e) => {
+    const filterValue = e.target.value;
+  
+    filterTodos(filterValue);
 });
-
-document.addEventListener
