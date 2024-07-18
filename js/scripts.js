c38ee3b9ff299c0d48c01c776a72511246bd0be8
Wothon-Mateus <13wothon@gmail.com>
2024-07-18 19:58:10 -0300

    Função save todo

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,42 @@ const filterBtn = document.querySelector("#filter-select");
 
 // Funções
 
-const saveTodo = (text) => {
+const saveTodo = (text, done = 0, save = 1) => {
     const todo = document.createElement("div");
     todo.classList.add("todo");
-
-    
-
-}    
+  
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+  
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+  
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+  
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleteBtn);
+  
+    // Utilizando dados da localStorage
+    if (done) {
+      todo.classList.add("done");
+    }
+  
+    if (save) {
+      saveTodoLocalStorage({ text, done: 0 });
+    }
+  
+    todoList.appendChild(todo);
+  
+    todoInput.value = "";
+  };  
 
 // Eventos
 
